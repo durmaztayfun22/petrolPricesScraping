ed43954253484b790c67f2e978caac395409ab4e
lovis durmaz <durmaztayfun178@gmil.com>
2024-11-05 19:04:22 +0300

    total data timeout organised-6

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; 
-const https = require("https");
-const agent = new https.Agent({ rejectUnauthorized: false });
 
 app.use(cors()); // CORS'u etkinleştir
 
@@ -137,18 +134,23 @@ const fetchOpetData = async () => {
 // });
 
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+const https = require("https");
+const agent = new https.Agent({ rejectUnauthorized: false });
 const totallink = "https://apimobiletest.oyakpetrol.com.tr/exapi/fuel_prices";
 
 app.get("/total-prices", async (req, res) => {
   try {
     const response = await axios.get(totallink, { httpsAgent: agent });
     const data = response.data;
+
     res.json(data); // Filtrelenmiş veriyi JSON olarak döndür
   } catch (error) {
     console.error(error);
     res.status(500).send("Error occurred while fetching TOTAL prices");
   }
 });
+
 
 
 // Döviz verilerini almak için endpoint
